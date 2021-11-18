de9379a035619b4afa86ea4011e14d472881f9b9
MarioFantasia <mariofantasia18@outlook.it>
2021-11-18 19:51:18 +0100

    tabella tag migrate

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,6 @@ Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->
     Route::get('/', 'HomeController@index')->name('home');
     Route::resource("posts", "PostController");
     Route::resource("categories", "CategoryController");
+    Route::resource("tags", "TagController");
+
 });
